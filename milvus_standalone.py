082cf6f0770367896701f022adede2c3f52b4dd3
awaisnazir08 <owaisnazir2228@gmail.com>
2024-07-11 19:29:04 +0500

    implement funtions to store data in batches

--- milvus_standalone.py
+++ milvus_standalone.py
@@ -1,10 +1,12 @@
-from pymilvus import connections, utility, MilvusException
+from pymilvus import connections, utility, MilvusException, db
 
 connections.connect(host="localhost", port="19530")
 
 try:
     # List all collections
     collections = utility.list_collections()
+    databases = db.list_database()
+    print(f'List all Databases: \n', databases)
     print(f"List all collections:\n", collections)
 except MilvusException as e:
     print(e)
